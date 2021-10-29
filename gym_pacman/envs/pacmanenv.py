0bc0257c9695f9fac93dd4b0be27683a6c0253b7
larpe17 <larpe17@student.sdu.dk>
2021-10-29 21:10:59 +0200

    updated rewards

--- gym_pacman/envs/pacmanenv.py
+++ gym_pacman/envs/pacmanenv.py
@@ -80,18 +80,18 @@ class PacmanEnv(Env):
     def _get_reward(self):
         self.done = False
         if self.game.events_AI == 0: # nothing or wall 
-            return -1
+            return -5
         elif self.game.events_AI == 1: # Pellets
-            return 2
+            return 5
         elif self.game.events_AI == 2: # super pellets
-            return 20
+            return 30
         elif self.game.events_AI == 3: # ghost kill
-            return 50
+            return 60
         elif self.game.events_AI == 4: # pacman dead
-            return -10
+            return -60
         elif self.game.events_AI == 5: # gameover
             self.done = True
-            return -50
+            return -100
         elif self.game.events_AI == 6: #  won
             self.done = True
             return 100
